85a61b391f78a0aa197d648ff962ad5655e12857
smae1993 <smae1993@gmail.com>
2024-07-09 12:02:57 +0330

    Initialize

--- karmui.jsx
+++ karmui.jsx
@@ -1,12 +1,8 @@
 // import { CacheProvider } from "@emotion/react";
-import { ThemeProvider } from '@mui/material';
+import { ThemeProvider } from "@mui/material";
 // import theme from "./theme";
 import { SnackbarProvider } from "notistack";
 
-
-
-export default function Karmui({children, theme}) {
-    return <ThemeProvider theme={theme}>
-            {children}
-        </ThemeProvider>
+export default function Karmui({ children, theme }) {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
